97e0db5b81fa4d649ddeb3cb58a80f7d85d986e5
cat_young <cyoun002@gmail.com>
2025-08-04 13:25:55 -0400

    Corrected line 7 to add strftime

--- using-modules-to-shift-time/main.py
+++ using-modules-to-shift-time/main.py
@@ -4,7 +4,7 @@ current_time = datetime.datetime.now()
 
 print(current_time.strftime("%m%d%y"))
 
-print(current_time("%B %d, %Y"))
+print(current_time.strftime("%B %d, %Y"))
 
 print(current_time.strftime("%B %d, %Y %I:%M %p"))
 
