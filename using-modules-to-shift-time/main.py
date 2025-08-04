b751c110f54728ae00f9077e91c5f33266793e8d
cat_young <cyoun002@gmail.com>
2025-08-04 13:15:48 -0400

    Added two different formats for dates

---
+++ using-modules-to-shift-time/main.py
@@ -0,0 +1,14 @@
+import datetime
+
+current_datetime = datetime.now()
+
+formatted_time = current_datetime.strftime("%m%d%y")
+#08/04/2025
+
+print(00/00/00)
+
+formatted_time = current_datetime.strftime("%B %d, %Y")
+#August 4, 2025
+
+print(formatted_time)
+
