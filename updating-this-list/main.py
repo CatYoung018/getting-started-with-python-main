91a084443b20c5b35b1a94f08ab93001be3502b2
cat_young <cyoun002@gmail.com>
2025-08-13 12:30:53 -0400

    Created 4 lists using slicing

---
+++ updating-this-list/main.py
@@ -0,0 +1,11 @@
+friends_list = ["Axel", "Marta", "Tyra", "Shazia", "Susan", "Dorothee", "Nelson", "Cathy", "Peggy", "Arnold", "Adam", "Cody", "Mason", "Kajuana", "Justin"]
+friends_list[:4]    
+print(friends_list[:4]) # Output: ['Axel', 'Marta', 'Tyra', 'Shazia']
+friends_list[11:]   
+print(friends_list[11:]) # Output: ['Cody', 'Mason', 'Kajuana', 'Justin']
+friends_list[10:14]
+print(friends_list[10:14]) # Output: ['Arnold', 'Adam', 'Cody', 'Mason']
+friends_list[7]
+print(friends_list[7]) # Output: 'Cathy'
+
+                
