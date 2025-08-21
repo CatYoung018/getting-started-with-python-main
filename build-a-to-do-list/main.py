aed107e4680f6fe56716ff2c2b451c71ef702998
cat_young <cyoun002@gmail.com>
2025-08-21 13:00:14 -0400

    Created while loop for todo list

--- build-a-to-do-list/main.py
+++ build-a-to-do-list/main.py
@@ -3,3 +3,11 @@ done = False
 
 print("Type 'done' at any time to exit")
 
+while not done:
+    task = new_item = input("Please add an item to your todo list:")
+    if new_item == "done":
+        done = True
+    else:
+        todo_list.append(new_item)
+        
+print("Your todo list:")      
