6ea69f317a5f7302d0b7c72f024a1337f5c9894f
cat_young <cyoun002@gmail.com>
2025-08-21 19:21:26 -0400

    Print new items added to the todo_list until done is typed

--- build-a-to-do-list/main.py
+++ build-a-to-do-list/main.py
@@ -4,15 +4,13 @@ done = False
 print("Type 'done' at any time to exit")
 
 while not done:
-    task = new_item = input("Please add an item to your todo list:")
+    new_item = input("Please add an item to your todo list:")
     if new_item == "done":
         done = True
     else:
         todo_list.append(new_item)
-
-
-for item in todo_list:
-    print("Your todo list:" + " " + item)
+    
+    print(todo_list)
 
 
 
