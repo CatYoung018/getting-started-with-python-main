a4c0a10be80cf5d3ccfead32751545cb1e2febdf
cat_young <cyoun002@gmail.com>
2025-10-02 13:19:07 -0400

    Enhanced dictionary

--- build-a-language-translator/main.py
+++ build-a-language-translator/main.py
@@ -1,9 +1,23 @@
 translations = {
   "hello":"hola",
   "thank you":"gracias",
-  "sorry":"lo siento"
+  "sorry":"lo siento",
+  "to live":"vivir",
+  "to write":"escribir"
 }
 
 done = False
 
 print('Type "done" at any time to exit')
+
+while not done:
+    word = input()
+    word.lower()
+
+    if word == "done":
+        done = True
+    elif word in translations:
+        print(translations[word])
+    else:
+      print("The word is not listed in this dictionary.")
+        
